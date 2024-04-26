c11e59ef7bdd20b2092380ca5f2d87c1e2742dd1
henriqueba2 <henrique.sbastos02@gmail.com>
2024-04-26 20:39:50 -0300

    javaSpring

---
+++ catracabackend/src/main/java/br/edu/eteczl/catracabackend/CatracabackendApplication.java
@@ -0,0 +1,13 @@
+package br.edu.eteczl.catracabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatracabackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatracabackendApplication.class, args);
+	}
+
+}
---
+++ catracabackend/src/test/java/br/edu/eteczl/catracabackend/CatracabackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.eteczl.catracabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatracabackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
